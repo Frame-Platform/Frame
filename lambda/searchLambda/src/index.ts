6cb55e4d96028691887cb944cfe6b7d099bc3e2e
Ralph Delia <Ralphdelia1@gmail.com>
2025-03-24 13:49:59 -0400

    create search lambda

---
+++ lambda/searchLambda/src/index.ts
@@ -0,0 +1,62 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { payloadSchema, TitanInputType } from "./types";
+import { ZodError } from "zod";
+
+import { downloadImage, resizeImageToLimit, callTitan } from "./utils";
+
+export const handler = async (
+  event: APIGatewayProxyEvent,
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const validationResult = payloadSchema.parse(event.body);
+    const { url, desc, threshold, topK } = validationResult;
+
+    // process image
+    let imagePayload = {};
+    if (url) {
+      const imageBuffer = await downloadImage(url);
+      const resizedBuffer = await resizeImageToLimit(imageBuffer);
+      imagePayload = { inputImage: resizedBuffer.toString("base64") };
+    }
+
+    // gather titan inputs
+    const textPayload = desc ? { inputText: desc } : {};
+    const payload: TitanInputType = { ...imagePayload, ...textPayload };
+
+    // call titan model for embeddings
+    const res = await callTitan(payload);
+    const embedding = res.embedding;
+
+    // query RDS Postgres PGVector
+    const rdsResults = {};
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(rdsResults),
+    };
+  } catch (e) {
+    console.log(e);
+    if (e instanceof ZodError) {
+      const errorMessages = e.errors.map((err) => err.message).join(", ");
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: "Validation error",
+          details: errorMessages,
+        }),
+      };
+    }
+
+    if (e instanceof Error) {
+      return {
+        statusCode: 400,
+        body: e.message,
+      };
+    }
+
+    return {
+      statusCode: 500,
+      body: "Unknown Server Error",
+    };
+  }
+};
---
+++ lambda/searchLambda/src/utils.ts
@@ -0,0 +1,57 @@
+import sharp from "sharp";
+import {
+  BedrockRuntimeClient,
+  InvokeModelCommand,
+} from "@aws-sdk/client-bedrock-runtime";
+import { TitanInputType } from "./types";
+
+export const callTitan = async (payload: TitanInputType) => {
+  try {
+    const bedrockClient = new BedrockRuntimeClient({ region: "us-east-1" });
+    const command = new InvokeModelCommand({
+      modelId: "amazon.titan-embed-image-v1",
+      body: JSON.stringify(payload),
+    });
+    const responseBedrock = await bedrockClient.send(command);
+    return await JSON.parse(Buffer.from(responseBedrock.body).toString());
+  } catch (e) {
+    throw new Error(`Error calling Titan, Error: ${e}`);
+  }
+};
+
+export const downloadImage = async (url: string) => {
+  let res;
+  try {
+    res = await fetch(url);
+  } catch (e) {
+    throw Error(`Failed downloading image ${url}, Error: ${e}`);
+  }
+
+  if (!res.ok) {
+    throw new Error(
+      `Non 200 response for url ${url}, status:${res.status} ${res.statusText}`,
+    );
+  }
+  const contentType = res.headers.get("content-type");
+  if (!contentType || !["image/png", "image/jpeg"].includes(contentType)) {
+    throw new Error(`Invalid content-type ${contentType} for url ${url}.`);
+  }
+
+  const arrayBuffer = await res.arrayBuffer();
+  return Buffer.from(arrayBuffer);
+};
+
+export const resizeImageToLimit = async (imageBuffer: Buffer) => {
+  const MAX_DIMENSION = 2048;
+  try {
+    return await sharp(imageBuffer)
+      .resize({
+        width: MAX_DIMENSION,
+        height: MAX_DIMENSION,
+        fit: "inside",
+      })
+      .toBuffer();
+  } catch (e) {
+    throw new Error(`Sharp resizing error: ${e}`);
+  }
+};
