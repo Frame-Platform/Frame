02aa6a5b89517eb0625494ab95a10fe990ce806d
Ralph Delia <Ralphdelia1@gmail.com>
2025-03-26 17:22:51 -0400

    multipart form data upload working

--- lambda/searchLambda/src/types.ts
+++ lambda/searchLambda/src/types.ts
@@ -3,14 +3,13 @@ import z from "zod";
 export const payloadSchema = z
   .object({
     url: z.string().url().optional(),
-    base64EncodedImage: z.string().optional(),
     desc: z.string().optional(),
     threshold: z.number().default(0),
     topK: z.number().default(10),
   })
-  .refine((data) => data.base64EncodedImage || data.url || data.desc, {
-    message: "Either url, base64encodedImage or desc must be provided",
-    path: ["base64EncodedImage", "url", "desc"],
+  .refine((data) => data.url || data.desc, {
+    message: "Either url or desc must be provided",
+    path: ["url", "desc"],
   });
 
 const titanInputSchema = z.object({
