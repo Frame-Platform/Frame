c4b4a2dd164486ad1227d3e8f363e93416caffb0
Chase Philpot <philpotchase@gmail.com>
2025-03-29 13:10:48 -0400

    added callGetDocuments function to index.ts, added first getDocsSchema to types.ts

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,11 +1,40 @@
+import * as Types from "./types";
+
 const BASE_URL = "https://lyy24w2m32.execute-api.us-east-1.amazonaws.com/testStage"
 const API_KEY = "gHEQTYyjZP3FKHxWDIrwoa5bSbsDB11b9CCIPcW8";
+// ^ npm install dotenv ? for .env to store API_key safely ?
+// dotenv.config() ?
 
-async function callGetDocuments(limit: string, offset: string) {
+async function callGetDocuments(
+  {limit, offset} : Types.getDocsSchema
+) {
+  let response;
 
+  try {
+    response = await fetch(`${BASE_URL}/document`, {
+      headers: {
+        "x-api-key": API_KEY,
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status} - ${response.statusText}`);
+    }
+
+    const json = await response.json();
+    console.log("Response JSON:", json);
+    // return json;
+  } catch (error: unknown) {
+    console.error("Error in callGetById:", error);
+    throw new Error(`Error fetching documents: ${error instanceof Error ? error.message : error}`);
+  }
+
+  return response;
 }
 
-async function callQImagesForEmbed(id: number | string) {
+callGetDocuments({ limit: '1', offset: '10'});
+
+async function callQImagesForEmbed() { // no parameters
 
 }
 
@@ -36,9 +65,9 @@ async function callGetById(id: number | string) {
   return response;
 }
 
-callGetById(39)
+// callGetById(39)
 
-async function callSearch() {
+async function callSearch() { // no parameters
 
 }
 
