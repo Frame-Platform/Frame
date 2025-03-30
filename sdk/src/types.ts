e870b04fc89576afa5e1c303ba739aea89895e22
Chase Philpot <philpotchase@gmail.com>
2025-03-30 12:51:18 -0400

    cleaned up types file, reformatted callGetDocuments, added OOP style SDK class to bottom of index.ts file, removed test file.

--- sdk/src/types.ts
+++ sdk/src/types.ts
@@ -1,5 +1,3 @@
-// import z from "zod"   ???
-
 export interface Document {
   url: string,
   desc?: string,
@@ -9,55 +7,15 @@ export interface Documents {
   documents: Document[]
 }
 
-export interface GetDocsParameterSchema {
+export interface GetDocsParameter {
   limit: string,
   offset: string,
 }
 
-export interface GetDocsReturnSchema {
+export interface GetDocsReturn {
   documents: Documents,
   limit: number,
   offset: number,
   total: number,
 }
 
-export interface GetDocsErrorSchema {
-  error: string,
-}
-
-export interface ImagesSchema {
-
-}
-
-
-/*
-
-export const getDocsSchema = z.object({
-  limit: z.string(),
-  offset: z.string(),
-})
-
-
- */
-/*
-
-export const payloadSchema = z
-  .object({
-    url: z.string().url().optional(),
-    desc: z.string().optional(),
-    threshold: z.number().default(0),
-    topK: z.number().default(10),
-  })
-  .refine((data) => data.url || data.desc, {
-    message: "Either url or desc must be provided",
-    path: ["url", "desc"],
-  });
-
-const titanInputSchema = z.object({
-  inputImage: z.string().optional(),
-  inputText: z.string().optional(),
-});
-
-export type TitanInputType = z.infer<typeof titanInputSchema>;
-
-*/
