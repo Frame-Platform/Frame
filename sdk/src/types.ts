9133af6b777c771bf69367faf26c97d10f49fc45
Chase Philpot <philpotchase@gmail.com>
2025-03-31 09:39:50 -0400

    New types, more robust error handling for deleteDocById

--- sdk/src/types.ts
+++ sdk/src/types.ts
@@ -19,3 +19,29 @@ export interface GetDocsReturn {
   total: number,
 }
 
+export interface DocumentResponse {
+  document: {
+    id: string;
+    url?: string;
+    desc?: string;
+  };
+  success: boolean;
+  message?: string;
+}
+
+export interface DocumentsResponse {
+  documents: Document[];
+  limit: string;
+  offset: string;
+  total: number;
+}
+
+export interface DeleteDocumentResponse {
+  document: {
+    url: string;
+    desc: string;
+    id: number | string;
+  };
+  success: boolean;
+  message: string;
+}
